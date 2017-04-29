05c77616535271b6af76d15120a17b7b1b00f35f
Mariana <marianalino@Air-de-Lino-2.home>
2017-04-30 00:42:02 +0100

    construtores do Account

--- Account.java
+++ Account.java
@@ -7,3 +7,28 @@ public class Account {
 	private String homeAddress;
 	private String birthday;
 }
+
+// contructors
+public Account(){
+	this.email = null;
+	this.name = null;
+	this.password = null;
+	this.homeAddress = null;
+	this.birthday = null;
+}
+
+public Account( String email, String name, String password, String homeAddress, String birthday){
+    this.email = email;
+    this.name = name;
+    this.password = password;
+    this.homeAddress = homeAddress;
+    this.birthday = birthday;
+}
+
+public Account (Account a){
+    this.email = a.getemail();
+    this.name = a.getname();
+    this.password = a.password();
+    this.
+
+}
