ee7d9683dc6d6a3af0d6f860793c58173c6790c9
Mariana <marianalino@Air-de-Lino-2.home>
2017-04-30 01:33:36 +0100

    gets,sets,clone,toString,hashcode,compareTo do Account

--- Account.java
+++ Account.java
@@ -6,9 +6,9 @@ public class Account {
 	private String password;
 	private String homeAddress;
 	private String birthday;
-}
 
-// contructors
+
+// contrutores
 public Account(){
 	this.email = null;
 	this.name = null;
@@ -26,9 +26,95 @@ public Account( String email, String name, String password, String homeAddress, 
 }
 
 public Account (Account a){
-    this.email = a.getemail();
-    this.name = a.getname();
-    this.password = a.password();
-    this.
+    this.email = a.getEmail();
+    this.name = a.getName();
+    this.password = a.getPassword();
+    this.homeAddress = a.getHomeAddress();
+    this.birthday =a.getBirthday();
+}
+
+//gets 
+
+public String getEmail(){
+    return this.email;
+}
+
+public String getName(){
+    return this.name;
+}
+
+public String getPassword(){
+    return this.password;
+}
+
+public String getHomeAddress(){
+    return this.homeAddress;
+}
+
+public String getBirthday(){
+    return this.birthday;
+}
+
+//sets 
+
+public void setEmail (String e){
+    this.email = e;
+}
+
+public void setName (String n){
+    this.name = n;
+}
+
+public void setPassword (String pw){
+    this.password = pw;
+}
+
+public void setHomeAddress (String ha){
+    this.homeAddress = ha;
+}
+
+public void setBirthday (String b){
+    this.birthday = b;
+}
+
+//clone, equals, toString, hashcode, compareTo
+
+public Account clone(){
+	    return new Account(this);
+}
+
+public boolean equals(Object o){
+    if (o==this) return true;
+    if (o==null) || (o.getClass()!=this.getClass())) return false;
+    Account a = (Account)o;
+        return (this.email.equals(a.getEmail()) && this.name == a.getName() && this.password == a.getPassword() && this.homeAddress == a.getHomeAddress() && this.birthday.equals(a.getBirthday()));
+}
+
+public String toString(){
+     StringBuilder r = new StringBuilder();
+     r.append("Email: ").append(this.email).append("\n");
+     r.append("Name: ").append(this.name).append("\n");
+     r.append("Password: ").append(this.password).append("\n");
+     r.append("HomeAddress: ").append(this.homeAddress).append("\n");
+     r.append("Birthday: ").append(this.birthday).append("\n");
+	    return r.toString();
+}
+
+public int hashCode{
+    int r=7;
+               
+    r = r*11 + this.email.hashCode();
+    r = r*11 + this.name.hashcode();
+    r = r*11 + this.password.hashcode();
+    r = r*11 + this.homeAddress.hashcode();
+    r = r*11 + this.birthday.hashcode();
+        return r;
+}
+
+public int compareTo (Account a){
+        return this.name.compareTo(a.getName());
 
 }
+
+
+
