086631fe9cf1d7bdb7d205d122c153d616fccc10
Mariana <marianalino@Air-de-Lino-2.home>
2017-04-30 01:37:11 +0100

    construtores do Client

--- Client.java
+++ Client.java
@@ -5,4 +5,23 @@ public class Client extends Account {
 	//instance variavels
 	private Point2D location;
 	private ArrayList<Travel> travels;  
+
+
+// contrutores
+public Client(){
+	this.location = (0,0);
+	this.travels = null;
+	
 }
+
+public Client( Point2D location, ArrayList<Travel> travels){
+    this.location = location;
+    this.travels = travels;
+}
+
+public Client (Client c){
+    this.location = c.getLocation();
+    this.travels = c.getTravels();
+   
+}
+
