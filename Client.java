ca5271e06231758347abc304dbfc7d048abbae55
Mariana <marianalino@Air-de-Lino-2.home>
2017-04-30 01:51:49 +0100

    gets,sets, clone, equals, toString, hashCode do Client

--- Client.java
+++ Client.java
@@ -7,19 +7,74 @@ public class Client extends Account {
 	private ArrayList<Travel> travels;  
 
 
-// contrutores
-public Client(){
-	this.location = new Point2D();
-	this.travels = new ArrayList<Travel>();
+    // contrutores
+    public Client(){
+	    this.location = new Point2D();
+	    this.travels = new ArrayList<Travel>();
+    }
+
+    public Client( Point2D location, ArrayList<Travel> travels){
+    	this.location = location.clone();
+    	this.travels = travels.clone();
+	}
+
+	public Client (Client c){
+    	this.location = c.getLocation();
+    	this.travels = c.getTravels();
+	}
+
+	//gets
+
+	public Point2D getLocation(){
+			return this.location.clone();
+	}
+
+	public ArrayList<Travel> getTravels(){
+			return this.travels.clone();
+	}
+	
+	//sets
+
+	public void setLocation(Point2D l){
+		this.location = l.clone();
+	}
+
+	public void setTravels(Driver t){
+		this.travels = t.clone();
+	}
+
+	//clone, equals, toString, hashCode
+
+	public Client clone(){
+			return new Client(this);
+	}
+
+	public boolean equals(Object o){
+			if(o==this) return true;
+			if((o==null) || (o.getClass()!=this.getClass())) return false;
+			Client c = (Client)o;
+			return (this.location.equals(c.getLocation()) && this.travels.equals(c.getTravels()));
+	}
+
+	public String toString(){
+            StringBuilder r = new StringBuilder();
+                
+		    r.append("Location: ").append(this.location.toString()).append("\n");
+		    r.append("Travels: ").append(this.travels.toString()).append("\n");
+            return r.toString();
+    }
+
+    public int hashCode(){
+            int r=7;
+                
+		
+            r = r*11 + this.location.hashCode();
+		    r = r*11 + this.travels.hashCode();
+            return r;
+    }
+
 }
 
-public Client( Point2D location, ArrayList<Travel> travels){
-    this.location = location.clone();
-    this.travels = travels.clone();
-}
 
-public Client (Client c){
-    this.location = c.getLocation();
-    this.travels = c.getTravels();
-}
+
 
