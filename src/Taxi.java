b0f7306a1a8dceb6d3b0aa9266318606723add79
MiguelQ <miguelquaresma97@gmail.com>
2017-05-03 11:45:54 +0100

    Taxi re-implementend as an abstract class

--- src/Taxi.java
+++ src/Taxi.java
@@ -1,10 +1,8 @@
-//import java.util.ArrayDeque;
 import java.util.List;
 
-public class Taxi extends App{
+public abstract class Taxi extends App{
 	
 	//instace variables
-	private String taxiType;
 	private String plate;
 	private double averageSpeed;
 	private double pricePerKm;
@@ -12,50 +10,8 @@ public class Taxi extends App{
 	private Point2D location;
 	private Driver driver;
 	List<Travel> tReg; 
-	/*
-	private boolean haveWaitList; //TODO
-	private ArrayDeque<Travel????> waitingList; //TODO
-	*/
 
-	//Constructors
-	public Taxi(){
-		this.taxiType = null;
-		this.plate = "";
-		this.averageSpeed = 0;
-		this.pricePerKm = 0;
-		this.reliability = 0;
-		this.location = new Point2D();
-		this.driver = new Driver();
-		this.tReg = new ArrayList<>();
-	}
-
-	public Taxi(String taxiType, String plate, double averageSpeed, double pricePerKm, double reliability, Point2D location, Driver driver, List<Travel> tReg){
-		this.taxiType = taxiType;
-		this.plate = plate;
-		this.averageSpeed = averageSpeed;
-		this.pricePerKm = pricePerKm;
-		this.reliability = reliability;
-		this.location = location.clone();
-		this.driver = driver.clone();
-		this.tReg = tReg.stream().map(Travel::clone).collect(Collectors.toCollection(ArrayList::new));
-	}
-
-	public Taxi(Taxi t){
-		this.taxiType = t.getTaxiType();
-		this.plate = t.getPlate();
-		this.averageSpeed = t.getAverageSpeed();
-		this.pricePerKm = t.getPricePerKm();
-		this.reliability = t.getReliability();
-		this.location = t.getLocation();
-		this.driver = t.getDriver();
-		this.tReg = t.getTReg();
-	}
-
-	//gets and sets
-	public String getTaxiType(){
-		return this.taxiType;
-	}
-
+    //gets and sets
 	public String getPlate(){
 		return this.plate;
 	}
@@ -82,10 +38,6 @@ public class Taxi extends App{
 	
 	public List<Travel> getTReg(){
 		return this.tReg.stream().map(Travel::clone).collect(Collectors.toCollection(ArrayList::new));
-	}
-
-	public void setTaxiType(String tt){
-		this.taxiType = tt;
 	}
 
 	public void setPlate(String nPl){
@@ -116,51 +68,36 @@ public class Taxi extends App{
 		this.tReg = nTReg.stream().map(Travel::clone).collect(Collectors.toCollection(ArrayList::new));
 	}
 
-	public Taxi clone(){
-		return new Taxi(this);
-	}
-
-	public boolean equals(Object o){
-		if(o==this) return true;
-		if((o==null) || (o.getClass()!=this.getClass())) return false;
-		Taxi t = (Taxi)o;
-		return (this.taxiType.equals(t.getTaxiType()) && this.averageSpeed == t.getAverageSpeed() && this.pricePerKm == t.getPricePerKm() && this.reliability == t.getReliability() && this.location.equals(t.getLocation()) &&  this.driver.equals(t.getDriver()));
-	}
-
-	public String toString(){
-                StringBuilder r = new StringBuilder();
-                r.append("TaxiType: ").append(this.taxiType).append("\n");
-                r.append("AverageSpeed: ").append(this.averageSpeed).append("\n");
-                r.append("PricePerKm: ").append(this.pricePerKm).append("\n");
-                r.append("Reliability: ").append(this.reliability).append("\n");
+    public String toString(){
+        StringBuilder r = new StringBuilder();
+        r.append("AverageSpeed: ").append(this.averageSpeed).append("\n");
+        r.append("PricePerKm: ").append(this.pricePerKm).append("\n");
+        r.append("Reliability: ").append(this.reliability).append("\n");
 		r.append("Location: ").append(this.location.toString()).append("\n");
 		r.append("Driver: ").append(this.driver.toString());
-                return r.toString();
-        }
+        return r.toString();
+    }
 
-        public int hashCode(){
-			int r=13;
-			long aux;
-			
-			r = r*23 + this.taxiType.hashCode();
-			aux = Double.doubleToLongBits(this.averageSpeed);
-			r = r*23 + (int)(aux ^ (aux >>> 32));
-			aux = Double.doubleToLongBits(this.pricePerKm);
-			r = r*23 + (int)(aux ^ (aux >>> 32));
-			aux = Double.doubleToLongBits(this.reliability);
-			r = r*23 + (int)(aux ^ (aux >>> 32));
-			r = r*23 + this.location.hashCode();
-			r = r*23 + this.driver.hashCode();
-			return r;
-        }
+    
+    public int hashCode(){
+        int r=13;
+        long aux;
+        
+        r = r*23 + this.taxiType.hashCode();
+        aux = Double.doubleToLongBits(this.averageSpeed);
+        r = r*23 + (int)(aux ^ (aux >>> 32));
+        aux = Double.doubleToLongBits(this.pricePerKm);
+        r = r*23 + (int)(aux ^ (aux >>> 32));
+        aux = Double.doubleToLongBits(this.reliability);
+        r = r*23 + (int)(aux ^ (aux >>> 32));
+        r = r*23 + this.location.hashCode();
+        r = r*23 + this.driver.hashCode();
+        return r;
+    }
 
-        public int compareTo(Taxi t){
-                return this.taxiType.compareTo(t.getTaxiType());
-        }
+    public abstract Taxi clone();
+    public abstract equals(Object o);
+    public abstract int compareTo(Taxi t);
+    public abstract void addTravel(Client c, Travel t);
 
-	public void addTravel(Client c, Travel t){
-		this.tReg.add(t.clone());
-		c.addTravel(t);
-		this.driver.addTravel(t);
-	}
 }
--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
 - [ ] public void requestRide() (TODO 1)
 - [ ] public void requestTaxi(String plate) (TODO 2)
 - [ ] public void bookTaxi(String plate) (TODO 3)
-- [ ] Implement Taxi as an abstract class (TODO 5)
+- [x] Implement Taxi as an abstract class (TODO 5)
 - [ ] Random trustworthiness generation after each ride (TODO 4)
 - [ ] Implement a (fifo) queue for taxis that support it
 - [ ] Rate driver after a ride 
